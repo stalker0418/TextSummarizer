15c200c257fee649956b99313e689df396bcd42f
stalker0418 <pakalapati.sanjay@gmail.com>
2024-07-10 17:25:18 -0400

    data ingestion both notebook and modular coding completed

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from textSummarizer.logging import logger
+# from textSummarizer.pipeline.stage_01_data_ingestion import dataIngestionTrainingPipeline
+from src.textSummarizer.logging import logger
 
-logger.info("This is a test log message")
+from src.textSummarizer.pipeline.stage_01_data_ingestion import dataIngestionTrainingPipeline
+
+pipleline_Stage_name = "Data Ingestion Stage"
+
+try:
+    logger.info(f"Starting {pipleline_Stage_name}")
+    data_ingestion = dataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"{pipleline_Stage_name} completed successfully")
+except Exception as e:
+    logger.exception(e)
+    raise e
